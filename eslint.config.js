1cac61c9d845821c3520858b2a90b33fc2a172ed
pickle288 <mp28@live.fr>
2026-03-17 21:03:45 +0100

    e

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+﻿import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "commonjs"
+    },
+    rules: {
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "no-console": "off",
+      "eqeqeq": ["error", "always"],
+      "curly": "error",
+      "quotes": ["error", "single", { avoidEscape: true }],
+      "semi": ["error", "always"],
+      "indent": ["error", 2],
+      "comma-dangle": ["error", "always-multiline"]
+    }
+  }
+];
