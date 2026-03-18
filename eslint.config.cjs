ca181e68f23a83d5b24483d36710383cda1f69bc
pickle288 <mp28@live.fr>
2026-03-18 18:59:20 +0100

    edit

---
+++ eslint.config.cjs
@@ -0,0 +1,30 @@
+﻿const js = require("@eslint/js");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "script", // ou "commonjs"
+      globals: {
+        __dirname: "readonly",
+        __filename: "readonly",
+        module: "readonly",
+        process: "readonly",
+        require: "readonly",
+        exports: "readonly"
+      }
+    },
+    rules: {
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "no-console": "off",
+      "eqeqeq": ["error", "always"],
+      "curly": "error",
+      "quotes": ["error", "single", { avoidEscape: true }],
+      "semi": ["error", "always"],
+      "indent": ["error", 2],
+      "comma-dangle": ["error", "always-multiline"]
+    }
+  }
+];
